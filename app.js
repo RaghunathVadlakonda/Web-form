c5be5dcf0e6d80717b5352dd0976aa826496fc79
Raghunath Vadlakonda <raghu@Raghunaths-MacBook-Air.local>
2019-02-19 14:55:33 +0530

    updated minor warnings and routes and also models

--- app.js
+++ app.js
@@ -1,22 +1,41 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
 const DATABASEURL = process.env.DATABASEURL;
 console.log(process.env.DBUSER, process.env.DBPASSWORD);
 const app = express();
 
+// route
+const userRoutes = require('./routes/user');
+
 //mongodb connection 
-mongoose.connect(DATABASEURL,{ useNewUrlParser : true});
+mongoose.connect(DATABASEURL,{ useNewUrlParser : true, useCreateIndex:true});
 
 // DeprecationWarning
 mongoose.Promise = global.Promise;
 
+//allows static files to be accessed publicly available
+app.use('/uploads',express.static('uploads'));
 
-//route
-app.get('/form', (req,res) => {
-    res.send('Welcome to web');
-})
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+
+// app.use((req,res,next)=>{
+//     //* will give access to any origin
+//  res.header('Access-Control-Allow-Origin','*');
+// //  res.header('Access-Control-Allow-Origin','Origin,X-Requested-With,Content-Type,Accept');
+// //  if(req.method === 'OPTIONS'){
+// //      res.header('Access-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET');
+// //      return res.status(200).json({});
+// //  }
+//  next();
+// });
+
+
+app.use('/form', userRoutes);
 
 
 console.log('started');
---
+++ routes/user.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const multer = require('multer');
+const User = require('../models/user');
+
+
+//set the storage engine
+const storage = multer.diskStorage ({
+    destination : './public/uploads',
+    filename: function(req, file, callback) {
+        req.newFileName = new Date().toISOString() + file.originalname;
+        callback(null , req.newFileName);
+    }
+});
+
+const fileFilter = (req, file, callback) => {
+    //reject a file
+    if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        callback(null, true);
+      } else {
+        callback(null, false);
+      }
+    };
+
+// stores in uploads folder
+const upload = multer({
+    storage: storage,
+    limits: {
+      fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter
+  });
+
+
+// route 
+//router.get();
+
+router.post('/form',upload.single('image'), (req, res, next) => {
+    const user = new User ({
+        name : req.body.name,
+        phonenumber : req.body.phonenumber,
+        email : req.body.email,
+        jobtitle : req.body.jobtitle,
+        image : req.newFileName,
+    })
+
+user
+.save()
+.then (result => {
+    console.log(result);
+    res.status(200).json({
+        message : 'success',
+        createdUser: user
+    });
+})
+.catch(err => {
+    console.log(err);
+    res.status(500).json({
+        error : err
+    });
+});
+});
+
+
+  module.exports = router;
