d057fc737a0241b4bbf0ddd3b888adc71a711ead
Raghunath Vadlakonda <raghu@Raghunaths-MacBook-Air.local>
2019-02-19 13:51:44 +0530

    created .env and connected server to database MLab

--- app.js
+++ app.js
@@ -1,15 +1,49 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const port = process.env.PORT || 5000;
+const DATABASEURL = process.env.DATABASEURL;
+console.log(process.env.DBUSER, process.env.DBPASSWORD);
 const app = express();
 
+//mongodb connection 
+mongoose.connect(DATABASEURL,{ useNewUrlParser : true});
+
+// DeprecationWarning
+mongoose.Promise = global.Promise;
+
+
+//route
+app.get('/form', (req,res) => {
+    res.send('Welcome to web');
+})
 
 
 console.log('started');
 
+
+//error handling
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status('404');
+    next(error); 
+});
+
+//error handling in application
+app.use((error, req, res, next)=> {
+    res.status(error.status || 500)
+    res.json ({
+        error :{
+            message : error.message
+        }
+    });
+});
 
 
 
 
 app.listen(port, (req, res) => {
     console.log('server started on port : 5000');
-})
+});
+
+module.exports = app;
---
+++ models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name : {
+        type: String,
+        required : true
+    },
+    phonenumber : {
+        type:Number,
+        required:true
+    },
+    email: {
+        type:String,
+        required:true,
+        unique:true,
+        match: /([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|"([]!#-[^-~ \t]|(\\[\t -~]))+")@([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\[[\t -Z^-~]*])/
+    },
+    jobtitle:{
+        type:String,
+        required:true
+    },
+    image:{
+        type:String,
+        required:true
+    }
+
+});
+
+module.exports = mongoose.model('User',userSchema);
+
+
