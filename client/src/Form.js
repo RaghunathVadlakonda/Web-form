f6a066c8edd0439c596f74b274d30e3a5a9e56c2
Raghunath Vadlakonda <raghu@Raghunaths-MacBook-Air.local>
2019-02-20 20:20:01 +0530

    updated client form

--- client/src/Form.js
+++ client/src/Form.js
@@ -1,38 +1,50 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './App.css';
+// import axios from 'axios'
 
-export default class Form extends Component {
-    //Intial State
-    state = {
-        Name: '',
-        PhoneNumber: '',
-        Email: '',
-        JobTitle: '',
-        Image: ''
-    };
-
-    // setstate - change value based on user input
-    change = e => {
-        this.setState({
-            [e.target.name] : e.target.value
-        });
-    };
-
-    onSubmit = (e) => {
-        e.preventDefault(); 
-        console.log(this.state);
+class Form extends Component {
+  constructor(){
+    super();
+    this.state={
+      name : '',
+      phonenumber : '',
+      email : '',
+      jobtitle: '',
+      image: ''
     }
+  }
+
+
+pleaseChange = (e)=>{
+    this.setState({
+        [e.target.name] : e.target.value
+    })
+}
+  
+pleaseSubmit = (e)=>{
+    e.preventDefault();
+    const data = this.state;
+    console.log(data);
+}
+
+componentDidMount(){
+    console.log(this.state)
+  }
+
   render() {
     return (
       <div>
         <form>
-            <input name = 'Name' placeholder = 'Name' value = {this.state.Name} onChange = {e => this.change(e)}  /> <br />
-            <input name = 'PhoneNumber' placeholder = 'PhoneNumber' type = 'number' value = {this.state.PhoneNumber} onChange = {e => this.change(e)}  /> <br />
-            <input name = 'Email' placeholder = 'Email' value = {this.state.Email} onChange = {e => this.change(e)}  /> <br />
-            <input name = 'JobTitle' placeholder = 'JobTitle' value = {this.state.JobTitle} onChange = {e => this.change(e)}  /> <br />
-            <input name = 'Image' placeholder = 'Name' type = 'file' value = {this.state.Image} onChange = {e => this.change(e)}  /> <br />
-            <button onClick = {e => this.onSubmit(e)}> Submit </button> 
+            <input type = 'text' name = 'name' value = {this.state.name} onChange={this.pleaseChange} /><br/><br/>
+            <input type = 'number' name = 'phonenumber' value = {this.state.phonenumber} onChange={this.pleaseChange}/><br/><br/>
+            <input type = 'email' name = 'email' value = {this.state.email} onChange={this.pleaseChange}/> <br/><br/>
+            <input type = 'text' name = 'jobtitle' value = {this.state.jobtitle} onChange={this.pleaseChange} /><br/><br/>
+            <input type = 'file' name = 'image' value = {this.state.image} onChange={this.pleaseChange}/> <br/><br/>
+            <button onSubmit={this.pleaseSubmit}> Submit </button>
         </form>
       </div>
-    )
+    );
   }
 }
+
+export default Form;
