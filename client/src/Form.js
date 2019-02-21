f82378a8a5e305b799c34447862fb02f304fb00b
Raghunath Vadlakonda <raghu@Raghunaths-MacBook-Air.local>
2019-02-21 15:38:27 +0530

    axios and updated some data

--- client/src/Form.js
+++ client/src/Form.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-// import axios from 'axios'
+import axios from 'axios'
 
 class Form extends Component {
   constructor(){
@@ -14,33 +14,72 @@ class Form extends Component {
     }
   }
 
+  fileChange=(e)=>{
+    console.log(e.target.files[0])
+  }
+
+
 
 pleaseChange = (e)=>{
     this.setState({
         [e.target.name] : e.target.value
     })
+    
 }
   
 pleaseSubmit = (e)=>{
-    e.preventDefault();
+    e.preventDefault(); 
     const data = this.state;
     console.log(data);
+    this.setState ({
+        name :'',
+        phonenumber :'',
+        email :'',
+        jobtitle:'',
+        image:'',
+    })
 }
 
 componentDidMount(){
     console.log(this.state)
   }
 
-  render() {
+
+fileUploadHandler = () => {
+    const data = new FormData();
+    data.append('name', this.state.name);
+    data.append('phonenumber', this.state.phonenumber);
+    data.append('email', this.state.email);
+    data.append('jobtitle', this.state.jobtitle);
+    data.append('image', this.state.image);
+
+    axios.post('/form', data, {
+        onUploadProgress : ProgressEvent => {
+            console.log('Upload Progress' +Math.round(ProgressEvent.loaded / ProgressEvent.total * 100) + '%')
+        }
+    })
+
+.then(res => {
+    console.log(res)
+})
+.catch((err) => {
+    console.log("Error in Uploading ", err)
+})
+}
+
+
+
+render() {
     return (
-      <div>
-        <form>
+      <div className = 'Form'>
+        <form onSubmit = {this.pleaseSubmit}>
             <input type = 'text' name = 'name' value = {this.state.name} onChange={this.pleaseChange} /><br/><br/>
             <input type = 'number' name = 'phonenumber' value = {this.state.phonenumber} onChange={this.pleaseChange}/><br/><br/>
             <input type = 'email' name = 'email' value = {this.state.email} onChange={this.pleaseChange}/> <br/><br/>
             <input type = 'text' name = 'jobtitle' value = {this.state.jobtitle} onChange={this.pleaseChange} /><br/><br/>
             <input type = 'file' name = 'image' value = {this.state.image} onChange={this.pleaseChange}/> <br/><br/>
-            <button onSubmit={this.pleaseSubmit}> Submit </button>
+            {/* <button onClick={this.fileUploadHandler}> Upload </button> */}
+            <button onClick={e => this.fileUploadHandler(e)}> Submit </button>
         </form>
       </div>
     );
